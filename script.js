949ca80f1c8a55b66293fecc5f2de3278c1304d2
A-coder152 <abdullahalsaleem19@gmail.com>
2025-11-05 20:55:09 -0500

    working tic tac toe

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 const cells = document.querySelectorAll(".cell")
+const restartBtn = document.getElementById("restart")
+const turnTracker = document.getElementById('turnTracker')
+
+const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+let turn = "X"
+let gameOver = false
 
 function cellClicked(event){
+    if (gameOver) {return}
     cell = event.currentTarget
-    cell.textContent = "x"
+    cell.textContent = turn
+    findWin()
+    if (gameOver) {return}
+    turn = (turn == "X") ? "O": "X"
+    turnTracker.textContent = `${turn}'s turn`
+}
+
+function findWin(){
+    winLines.forEach((line) => {
+        let cell1 = cells[line[0]]
+        let cell2 = cells[line[1]]
+        let cell3 = cells[line[2]]
+        if (cell1.textContent == cell2.textContent && cell3.textContent && turn && cell1.textContent == cell3.textContent){
+            turnTracker.textContent = `${turn} wins!`
+            gameOver = true
+        }
+        console.log(cell1.textContent, cell2.textContent, cell3.textContent, turn)
+    })
 }
 
 cells.forEach(cell => 
     cell.addEventListener("click", cellClicked)
 )
+
+restartBtn.addEventListener("click", () => {
+    cells.forEach(cell => cell.textContent = "")
+    gameOver = false
+    turn = "X"
+    turnTracker.textContent = "X's turn"
+})
