a752c2fe88a4fd4bdfcadd3a2231ca5156f8b147
A-coder152 <abdullahalsaleem19@gmail.com>
2025-11-05 21:47:00 -0500

    now its rock paper scissors, help i didnt imagine its this easy what to do now

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cells = document.querySelectorAll(".cell")
 const restartBtn = document.getElementById("restart")
 const turnTracker = document.getElementById('turnTracker')
+const selectRock = document.getElementById('selectRock')
+const selectPaper = document.getElementById('selectPaper')
+const selectScissors = document.getElementById('selectScissors')
 
 const winLines = [
     [0, 1, 2],
@@ -12,14 +15,19 @@ const winLines = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+const beatsDict = {"R": "S", "P": "R", "S": "P"}
 
 let turn = "X"
 let gameOver = false
+let selectedMove = "R"
 
 function cellClicked(event){
     if (gameOver) {return}
     cell = event.currentTarget
-    cell.textContent = turn
+    if (cell.textContent) {
+        if (beatsDict[selectedMove] != cell.textContent) {return}
+    }
+    cell.textContent = selectedMove
     findWin()
     if (gameOver) {return}
     turn = (turn == "X") ? "O": "X"
@@ -31,7 +39,7 @@ function findWin(){
         let cell1 = cells[line[0]]
         let cell2 = cells[line[1]]
         let cell3 = cells[line[2]]
-        if (cell1.textContent == cell2.textContent && cell3.textContent && turn && cell1.textContent == cell3.textContent){
+        if (cell1.textContent == cell2.textContent && cell3.textContent && cell1.textContent == cell3.textContent){
             turnTracker.textContent = `${turn} wins!`
             gameOver = true
         }
@@ -49,3 +57,7 @@ restartBtn.addEventListener("click", () => {
     turn = "X"
     turnTracker.textContent = "X's turn"
 })
+
+selectRock.addEventListener("click", () => {selectedMove = "R"})
+selectPaper.addEventListener("click", () => {selectedMove = "P"})
+selectScissors.addEventListener("click", () => {selectedMove = "S"})
